67fa21a530a7f8becebc5c3ec72519a5656606a3
als92 <senkinas@gmail.com>
2021-03-28 14:40:06 +0100

    3.4

--- index.js
+++ index.js
@@ -46,5 +46,11 @@ app.get("/api/persons/:id", (req, res) => {
 	}
 })
 
+app.delete("/api/persons/:id", (req, res) => {
+	const id = Number(req.params.id)
+	note = persons.filter(person => person.id !== id)
+	res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
