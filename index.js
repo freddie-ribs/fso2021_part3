27520ef0e218b48995316c31213ea271cfab9bf0
als92 <senkinas@gmail.com>
2021-03-29 15:12:26 +0100

    3.10

--- index.js
+++ index.js
@@ -82,5 +82,5 @@ app.post("/api/persons", (req, res) => {
 	res.json(contact)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
